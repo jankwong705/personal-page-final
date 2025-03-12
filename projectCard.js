0cf8bf6c46e5d84049a247ed333eb3de111c16cd
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2025-03-11 22:01:41 -0700

    Loading project data into localStorage

--- projectCard.js
+++ projectCard.js
@@ -45,15 +45,15 @@ class ProjectCard extends HTMLElement {
 
     getComponentHTML(title, employer, location, time, imgSrc, altText, item1, item2, item3, link) {
         return `
-            <h2>${this.getAttribute('title') || 'NA'}</h2>
-            <i>${this.getAttribute('location') || 'NA'}</i> <br>
-            <i>${this.getAttribute('time') || 'NA'}</i> <br>
+            <h2>${this.getAttribute('title') || ''}</h2>
+            <i>${this.getAttribute('location') || ''}</i> <br>
+            <i>${this.getAttribute('time') || ''}</i> <br>
             <details id="job-details">
                 <summary id="job-details-sum">Details</summary>
                 <ul>
-                    <li>${this.getAttribute('item1') || 'NA'}</li>
-                    <li>${this.getAttribute('item2') || 'NA'}</li>
-                    <li>${this.getAttribute('item3') || 'NA'}</li>
+                    <li>${this.getAttribute('item1') || ''}</li>
+                    <li>${this.getAttribute('item2') || ''}</li>
+                    <li>${this.getAttribute('item3') || ''}</li>
                     <li> <a href=${this.getAttribute('link')}> <strong>Link to project: ${this.getAttribute('link')}</strong> </a> </li>
                 </ul>
             </details>
@@ -65,3 +65,39 @@ class ProjectCard extends HTMLElement {
 }
 
 customElements.define('project-card', ProjectCard);
+const loadLocal = document.getElementsByID("load-local");
+const loadRemote = document.getElementsByID("load-remote");
+
+loadLocal.addEventListener("click", () => {
+    
+}); 
+
+try {
+    const projectData = await JSON.parse(localStorage.getItem("projects")) 
+    projectData.record.forEach(car => {
+        console.log(car);
+        // Example of expected result:
+        // {
+        // image_url:
+        // "working_url_here"
+        // make:
+        // "Suzuki"
+        // model:
+        // "Wagon R"
+        // year:
+        // 1998
+        //}
+
+
+        window.galleryItem = document.createElement("my-ride");
+        // TODO 5: Set attributes of my-ride as follows:
+        // <!-- <my-ride year="1998" make="Suzuki" model="Wagon R"
+        //     image-url="https://pub-940f423c24074b9ebe0052cc5f384faa.r2.dev/IMG_4746.jpg"></my-ride> -->
+        // using the properties in the "car" obj.
+        // setAttribute() will do the trick.
+        document.getElementById("gallery").appendChild(window.galleryItem);
+    });
+
+} catch (error) {
+    console.error("Can't load projects!");
+}
